091714e804dd48d3e626d3e55e8b362a2dbfdfc0
John Hayes <johnnyhayes7@gmail.com>
2019-01-17 14:58:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,6 +36,7 @@ def hit?(total)
   if get_user_input == "h"
    total += deal_card
   #elsif get_user_input == "s"
+  total
   #elsif get_user_input != "h" || "s"
   #invalid_command
   #prompt_user
